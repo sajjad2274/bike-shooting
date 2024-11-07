aaca4df80f8fbdbfd85c7feb5d2b22c91bb2a9b5
ayeshagamedevdevevibe <ayeshagamedevdevevibe@gmail.com>
2024-11-07 13:42:01 +0500

    Ayesha

--- Assets/Simple Motocross Physics/Scripts/MotoCamera.cs
+++ Assets/Simple Motocross Physics/Scripts/MotoCamera.cs
@@ -1,116 +1,39 @@
-//using UnityEngine;
-//using System.Collections;
-//using SMPScripts;
-
-//    public class MotoCamera : MonoBehaviour
-//    {
-//         Transform target;
-//        public  bool  stuntCamera;
-//        private float distance = 4.5f;
-//        private float height = 2.81f;
-//        private float heightDamping = 100f;
-
-//        private float lookAtHeight = 1.87f;
-
-//        private float rotationSnapTime = 0.5f;
-
-
-//        private Vector3 lookAtVector;
-
-//        private float usedDistance;
-
-//        float wantedRotationAngle;
-//        float wantedHeight;
-
-//        float currentRotationAngle;
-//        float currentHeight;
-
-//        Vector3 wantedPosition;
-
-//        private float yVelocity = 0.0F;
-//        private float zVelocity = 0.0F;
-//        MotoPerfectMouseLook perfectMouseLook;
-
-
-
-//        void Start()
-//        {
-//            perfectMouseLook = GetComponent<MotoPerfectMouseLook>();
-//            if (stuntCamera)
-//            {
-//                var follow = new GameObject("Follow");
-//                var toFollow = GameObject.FindObjectOfType<MotoController>().transform;
-//                follow.transform.SetParent(toFollow);
-//                follow.transform.position = toFollow.position + toFollow.gameObject.GetComponent<BoxCollider>().center;
-//                target = GameObject.FindObjectOfType<MotoController>().transform;
-//                target = follow.transform;
-
-//                height -= toFollow.gameObject.GetComponent<BoxCollider>().center.y;
-//                lookAtHeight -= toFollow.gameObject.GetComponent<BoxCollider>().center.y;
-//            }
-//            else
-//                target = GameObject.FindObjectOfType<MotoController>().transform;
-//        }
-
-//        void LateUpdate()
-//        {
-//            if (target != null)
-//            {
-//                wantedHeight = target.position.y + height;
-//                currentHeight = transform.position.y;
-
-//                wantedRotationAngle = target.eulerAngles.y;
-//                currentRotationAngle = transform.eulerAngles.y;
-//                if (perfectMouseLook.movement == false)
-//                    currentRotationAngle = Mathf.SmoothDampAngle(currentRotationAngle, wantedRotationAngle, ref yVelocity, rotationSnapTime);
-
-//                currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.fixedDeltaTime);
-
-//                wantedPosition = target.position;
-//                wantedPosition.y = currentHeight;
-
-//                usedDistance = Mathf.SmoothDampAngle(usedDistance, distance, ref zVelocity, 0.1f);
-
-//                wantedPosition += Quaternion.Euler(0, currentRotationAngle, 0) * new Vector3(0, 0, -usedDistance);
-
-
-//            transform.position = wantedPosition;
-
-//                transform.LookAt(target.position + lookAtVector);
-
-//                lookAtVector = new Vector3(0, lookAtHeight, 0);
-//            }
-
-
-//        }
-
-//    }
-
-
 using UnityEngine;
 using System.Collections;
 using SMPScripts;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using Unity.Netcode;
 
-public class MotoCamera : MonoBehaviour
+public class MotoCamera : NetworkBehaviour
 {
     Transform target;
     public bool stuntCamera;
     private float distance = 4.5f;
     private float height = 2.81f;
     private float heightDamping = 100f;
+
     private float lookAtHeight = 1.87f;
+
     private float rotationSnapTime = 0.5f;
 
+
     private Vector3 lookAtVector;
+
     private float usedDistance;
+
     float wantedRotationAngle;
     float wantedHeight;
+
     float currentRotationAngle;
     float currentHeight;
+
     Vector3 wantedPosition;
+
     private float yVelocity = 0.0F;
     private float zVelocity = 0.0F;
     MotoPerfectMouseLook perfectMouseLook;
+
+
 
     void Start()
     {
@@ -120,11 +43,15 @@ public class MotoCamera : MonoBehaviour
         MotoController[] players = GameObject.FindObjectsOfType<MotoController>();
         foreach (MotoController player in players)
         {
-            if (player.isLocalPlayer)  // Assuming you set `isLocalPlayer` based on the network player
+            if (player.isLocalPlayer)  
             {
                 SetCameraTarget(player);
                 break;
             }
+            //if (player.GetComponent<NetworkObject>().IsLocalPlayer)
+            //{
+            //    cameraFollow.target = player.transform;
+            //}
         }
     }
 
@@ -164,12 +91,19 @@ public class MotoCamera : MonoBehaviour
             wantedPosition.y = currentHeight;
 
             usedDistance = Mathf.SmoothDampAngle(usedDistance, distance, ref zVelocity, 0.1f);
+
             wantedPosition += Quaternion.Euler(0, currentRotationAngle, 0) * new Vector3(0, 0, -usedDistance);
 
+
             transform.position = wantedPosition;
+
             transform.LookAt(target.position + lookAtVector);
 
             lookAtVector = new Vector3(0, lookAtHeight, 0);
         }
+
+
     }
+
 }
+
--- Assets/Simple Motocross Physics/Scripts/ShooterController.cs
+++ Assets/Simple Motocross Physics/Scripts/ShooterController.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Netcode;
 
-public class ShooterController : MonoBehaviour
+public class ShooterController : NetworkBehaviour
 {
     //[SerializeField] private Button fireButton;
     [SerializeField] private ParticleSystem muzleFlash;
